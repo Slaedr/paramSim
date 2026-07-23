437e231d30fabdf419de9d1cc3d147770f287d93
Aditya Kashi <kashia@ornl.gov>
2026-07-23 14:25:15 -0400

    formatting changes

--- cases/case.cpp
+++ cases/case.cpp
@@ -2,66 +2,72 @@
 
 #include <memory>
 
-#include "poisson/verify.hpp"
+#include "minimal_surface/gaussians.hpp"
+#include "minimal_surface/minimal_surface.hpp"
+#include "minimal_surface/verify.hpp"
 #include "poisson/exps.hpp"
 #include "poisson/fourier.hpp"
 #include "poisson/polynomial.hpp"
-#include "minimal_surface/verify.hpp"
-#include "minimal_surface/minimal_surface.hpp"
-#include "minimal_surface/gaussians.hpp"
+#include "poisson/verify.hpp"
 
 namespace paramsim {
 
-
 template <int dim>
-std::unique_ptr<Case<dim>> create_case(const CommonParams& params, const int n_args,
-                                       const char* const argv[])
+std::unique_ptr<Case<dim>> create_case(const CommonParams &params,
+                                       const int n_args,
+                                       const char *const argv[])
 {
     std::unique_ptr<Case<dim>> tcase;
-    if(params.case_str == "poisson_verify") {
+    if (params.case_str == "poisson_verify") {
         tcase = std::make_unique<cases::PoissonVerify<dim>>();
-    } else if(params.case_str == "poisson_bc_exp") {
+    } else if (params.case_str == "poisson_bc_exp") {
         tcase = std::make_unique<cases::PoissonBCExp<dim>>();
-    } else if(params.case_str == "poisson_bc_fourier") {
+    } else if (params.case_str == "poisson_bc_fourier") {
         tcase = std::make_unique<cases::PoissonBCFourier<dim>>();
-    } else if(params.case_str == "poisson_bc_polynomial") {
+    } else if (params.case_str == "poisson_bc_polynomial") {
         tcase = std::make_unique<cases::PoissonBCPolynomial<dim>>();
-    } else if(params.case_str == "minimal_surface_ball_verify") {
-        if constexpr(dim == 2) {
+    } else if (params.case_str == "minimal_surface_ball_verify") {
+        if constexpr (dim == 2) {
             tcase = std::make_unique<cases::MinSurfBallVerify<dim>>();
         } else {
             throw std::runtime_error(
                 "Case 'minimal_surface_ball_verify' does not support "
-                "spatial dimension " + std::to_string(dim) + ".");
+                "spatial dimension " +
+                std::to_string(dim) + ".");
         }
-    } else if(params.case_str == "minimal_surface_cube_verify") {
-        if constexpr(dim == 2) {
+    } else if (params.case_str == "minimal_surface_cube_verify") {
+        if constexpr (dim == 2) {
             tcase = std::make_unique<cases::MinSurfCubeVerify<dim>>();
         } else {
             throw std::runtime_error(
                 "Case 'minimal_surface_cube_verify' does not support "
-                "spatial dimension " + std::to_string(dim) + ".");
+                "spatial dimension " +
+                std::to_string(dim) + ".");
         }
-    } else if(params.case_str == "minimal_surface_disk_sinusoidal") {
+    } else if (params.case_str == "minimal_surface_disk_sinusoidal") {
         tcase = std::make_unique<cases::MinSurfDiskSinusoidal<dim>>();
-    } else if(params.case_str == "minimal_surface_cube_sinusoidal") {
+    } else if (params.case_str == "minimal_surface_cube_sinusoidal") {
         tcase = std::make_unique<cases::MinSurfCubeSinusoidal<dim>>();
-    } else if(params.case_str == "minimal_surface_cube_polynomial") {
+    } else if (params.case_str == "minimal_surface_cube_polynomial") {
         tcase = std::make_unique<cases::MinSurfCubePolynomial<dim>>();
-    } else if(params.case_str == "minimal_surface_cube_gaussians") {
+    } else if (params.case_str == "minimal_surface_cube_gaussians") {
         tcase = std::make_unique<cases::MinSurfCubeGaussians<dim>>();
     } else {
         throw std::runtime_error("Non-existent case!");
     }
-    bpo::options_description case_desc
-        (std::string("Solves the case ") + params.case_str + " given one set of parameters.");
+    bpo::options_description case_desc(std::string("Solves the case ") +
+                                       params.case_str +
+                                       " given one set of parameters.");
     tcase->add_case_cmd_args(case_desc);
-    const bpo::variables_map case_cmdmap = get_cmd_args(n_args, argv, case_desc);
+    const bpo::variables_map case_cmdmap =
+        get_cmd_args(n_args, argv, case_desc);
     tcase->initialize(case_cmdmap);
     return tcase;
 }
 
-template std::unique_ptr<Case<2>> create_case(const CommonParams&, int, const char *const[]);
-template std::unique_ptr<Case<3>> create_case(const CommonParams&, int, const char *const[]);
+template std::unique_ptr<Case<2>> create_case(const CommonParams &, int,
+                                              const char *const[]);
+template std::unique_ptr<Case<3>> create_case(const CommonParams &, int,
+                                              const char *const[]);
 
-}
+} // namespace paramsim
--- cases/poisson/exps.hpp
+++ cases/poisson/exps.hpp
@@ -13,74 +13,74 @@ namespace cases {
 
 namespace poisson_exp {
 
-  using namespace dealii;
+using namespace dealii;
 
-  template <int dim>
-  struct Params {
+template <int dim>
+struct Params {
     static constexpr int n_centers = 3;
 
     std::array<Point<dim>, n_centers> centers = [] {
-      std::array<Point<dim>, n_centers> values{};
-      values[0][0] = -1.0;
-      values[0][1] = -0.67;
-      values[1][0] = -1.0;
-      values[1][1] = -0.01;
-      values[2][0] = -1.0;
-      values[2][1] = 0.66;
-      return values;
+        std::array<Point<dim>, n_centers> values{};
+        values[0][0] = -1.0;
+        values[0][1] = -0.67;
+        values[1][0] = -1.0;
+        values[1][1] = -0.01;
+        values[2][0] = -1.0;
+        values[2][1] = 0.66;
+        return values;
     }();
 
     std::array<double, n_centers> coeffs{{0.27, 0.35, -0.34}};
-    
+
     /// Same width for all three hills
     double width{0.4};
 
     double gamma{get_multiplier()};
 
-    double get_multiplier() const {
+    double get_multiplier() const
+    {
         constexpr int profile_dim = 2;
-        return 1.0 / std::pow(2. * numbers::PI * width * width, profile_dim / 2.);
+        return 1.0 /
+               std::pow(2. * numbers::PI * width * width, profile_dim / 2.);
     }
 
-    Params() { }
+    Params() {}
 
-    Params(const std::array<Point<dim>, n_centers>& centerss,
-        const std::array<double, n_centers>& coefficients,
-        const double hill_width)
+    Params(const std::array<Point<dim>, n_centers> &centerss,
+           const std::array<double, n_centers> &coefficients,
+           const double hill_width)
         : centers{centerss}, coeffs{coefficients}, width{hill_width},
-        gamma{get_multiplier()}
-    { }
-  };
+          gamma{get_multiplier()}
+    {
+    }
+};
 
-
-  // The last function we implement is the right hand side for the
-  // manufactured solution.
-  template <int dim>
-  class RightHandSide : public Function<dim>
-  {
-  public:
+// The last function we implement is the right hand side for the
+// manufactured solution.
+template <int dim>
+class RightHandSide : public Function<dim> {
+public:
     static constexpr int n_centers = 1;
 
     virtual double value(const Point<dim> &p,
                          const unsigned int /*component*/ = 0) const override
     {
-      double sum = -0.0625;
-      for (int i = 0; i < n_centers; ++i)
-      {
-        const Tensor<1, dim> x_minus_xi = p - centers[i];
-        const double arg = -x_minus_xi.norm_square() / (width * width);
-        sum += coeffs[i] * std::exp(arg) *
-               (static_cast<double>(dim) / 2.0 + arg);
-      }
-      return sum * 4.0 * gamma / (width * width);
+        double sum = -0.0625;
+        for (int i = 0; i < n_centers; ++i) {
+            const Tensor<1, dim> x_minus_xi = p - centers[i];
+            const double arg = -x_minus_xi.norm_square() / (width * width);
+            sum += coeffs[i] * std::exp(arg) *
+                   (static_cast<double>(dim) / 2.0 + arg);
+        }
+        return sum * 4.0 * gamma / (width * width);
     }
 
-  private:
+private:
     std::array<Point<dim>, n_centers> centers = [] {
-      std::array<Point<dim>, n_centers> values{};
-      values[0][0] = -0.125;
-      values[0][1] = 0.125;
-      return values;
+        std::array<Point<dim>, n_centers> values{};
+        values[0][0] = -0.125;
+        values[0][1] = 0.125;
+        return values;
     }();
 
     std::array<double, n_centers> coeffs{{1.0}};
@@ -88,66 +88,61 @@ namespace poisson_exp {
     double width{0.5};
     double gamma{get_multiplier()};
 
-    double get_multiplier() const {
+    double get_multiplier() const
+    {
         return 1.0 / std::pow(2. * numbers::PI * width * width, dim / 2.);
     }
-  };
+};
 
-  template <int dim>
-  class DirichletIn : public Function<dim>
-  {
-  public:
-    DirichletIn()
-    { }
-    
-    DirichletIn(const Params<dim>& params) : params_{params}
-    { }
+template <int dim>
+class DirichletIn : public Function<dim> {
+public:
+    DirichletIn() {}
+
+    DirichletIn(const Params<dim> &params) : params_{params} {}
 
     virtual double value(const Point<dim> &p,
                          const unsigned int /*component*/ = 0) const override
     {
-      double sum = 0;
-      for (int i = 0; i < Params<dim>::n_centers; ++i)
-      {
-          // const Tensor<1, dim> x_minus_xi = p - params_.centers[i];
-          double distance_square = 0.0;
-          constexpr int profile_dim = 2;
-          for(int idim = 0; idim < profile_dim; ++idim) {
-            distance_square += std::pow(p[idim] - params_.centers[i][idim], 2);
-          }
-          // sum += params_.coeffs[i] *
-          //   std::exp(-x_minus_xi.norm_square() /
-          //            (params_.width * params_.width));
-          sum += params_.coeffs[i] *
-            std::exp(-distance_square / (params_.width * params_.width));
-      }
+        double sum = 0;
+        for (int i = 0; i < Params<dim>::n_centers; ++i) {
+            // const Tensor<1, dim> x_minus_xi = p - params_.centers[i];
+            double distance_square = 0.0;
+            constexpr int profile_dim = 2;
+            for (int idim = 0; idim < profile_dim; ++idim) {
+                distance_square +=
+                    std::pow(p[idim] - params_.centers[i][idim], 2);
+            }
+            // sum += params_.coeffs[i] *
+            //   std::exp(-x_minus_xi.norm_square() /
+            //            (params_.width * params_.width));
+            sum += params_.coeffs[i] *
+                   std::exp(-distance_square / (params_.width * params_.width));
+        }
 
-      return sum * params_.gamma;
+        return sum * params_.gamma;
     }
 
     const Params<dim> params_;
-  };
+};
 
-  template <int dim>
-  using DirichletConstant = cases::DirichletConstant<dim>;
-}
-
+template <int dim>
+using DirichletConstant = cases::DirichletConstant<dim>;
+} // namespace poisson_exp
 
 namespace bpo = boost::program_options;
 
-
 template <int dim>
-class PoissonBCExp final : public Case<dim>
-{
+class PoissonBCExp final : public Case<dim> {
 public:
-    void initialize(const bpo::variables_map&) override;
-    void add_case_cmd_args(bpo::options_description&) const override;
+    void initialize(const bpo::variables_map &) override;
+    void add_case_cmd_args(bpo::options_description &) const override;
 
 private:
     static const std::array<std::string, 3> dimnames;
 };
 
-}
-}
+} // namespace cases
+} // namespace paramsim
 
 #endif
