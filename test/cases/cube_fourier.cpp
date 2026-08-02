7c8a323a6a741c2963a6795ff4afeb49729ec10d
Aditya Kashi <kashia@ornl.gov>
2026-08-02 16:22:23 -0400

    added geometry and bc id tests for cube fourier case

--- test/cases/cube_fourier.cpp
+++ test/cases/cube_fourier.cpp
@@ -1,5 +1,9 @@
 #include <gtest/gtest.h>
 
+#include <deal.II/grid/tria.h>
+#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/fe/fe_q.h>
+
 #include "../../cases/cube/fourier.hpp"
 #include "../../pdes/nonlinear_elliptic/minimal_surface.hpp"
 #include "../../pdes/poisson/poisson_cg.hpp"
@@ -23,6 +27,36 @@ TEST(CubeFourierPoisson, SolverConvergesWithP1In3D)
                                    paramsim::pde::PoissonCG>(
                   8, 3, "poisson_cg", poisson_maximum_newton_iterations)),
               nonlinear_tolerance);
+}
+
+TEST(CubeFourier, GeometryIsUnitCube)
+{
+    auto test_case = std::make_shared<paramsim::cases::cube::CubeFourier<3>>();
+    test_case->initialize(boost::program_options::variables_map{});
+    auto geom = test_case->get_geometry();
+    ASSERT_TRUE(std::dynamic_pointer_cast<const paramsim::geom::Cube<3>>(geom));
+}
+
+TEST(CubeFourier, BoundaryTags)
+{
+    auto test_case = std::make_shared<paramsim::cases::cube::CubeFourier<3>>();
+    test_case->initialize(boost::program_options::variables_map{});
+    auto geom = test_case->get_geometry();
+    dealii::Triangulation<3> tria;
+    geom->generate_grid(tria, 3);
+    geom->set_boundary_ids(tria);
+    dealii::DoFHandler<3> dof_handler(tria);
+    dealii::FE_Q<3> fe(1);
+    dof_handler.distribute_dofs(fe);
+
+    for(const auto &cell : dof_handler.active_cell_iterators()) {
+        for(const auto &face : cell->face_iterators()) {
+            if(!face->at_boundary()) {
+                continue;
+            }
+            EXPECT_TRUE(face->boundary_id() == 1);
+        }
+    }
 }
 
 TEST(CubeFourierMinimalSurface, SolverConvergesWithP1In2D)
